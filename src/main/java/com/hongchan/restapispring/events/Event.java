552e9385057a070bb9217b9b6d4f9793465f0e40
hongchan2 <ipeter@naver.com>
2020-05-14 22:31:13 +0900

    Add create envents test

--- src/main/java/com/hongchan/restapispring/events/Event.java
+++ src/main/java/com/hongchan/restapispring/events/Event.java
@@ -17,7 +17,7 @@ public class Event {
     private LocalDateTime endEventDateTime;
     private String location; // (optional) 이게 없으면 온라인 모임
     private int basePrice; // (optional)
-    private int maxPrice; // (optional) private int limitOfEnrollment;
+    private int maxPrice; // (optional)
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
